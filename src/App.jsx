bfbd46d1232b0bd1e3a9709088e08cdf5b8dff7c
Quirax Lee <quiraxical@gmail.com>
2025-09-05 10:58:44 +0900

    Added main page (content is still in construction)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Page from './components/Page'
+import MainPage from './pages/MainPage'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <Page>
-                <a
-                    href='https://vite.dev'
-                    target='_blank'>
-                    <img
-                        src={viteLogo}
-                        className='logo float-right'
-                        alt='Vite logo'
-                    />
-                </a>
-                <a
-                    href='https://react.dev'
-                    target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </Page>
+            <MainPage />
             <h1>Vite + React</h1>
             <div className='card'>
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import Page from '../components/Page'
+
+export default () => (
+    <Page className='bg-black'>
+        <div>
+            {/* Top */}
+            <h1>謹弔</h1>
+            <h2>삼가 고인의 명복을 빕니다</h2>
+        </div>
+        <div>
+            {/* Bottom */}
+            {/* TODO: 이미지 추가 - 이츠카 시네 */}
+            {/* TODO: 이미지 추가 - 국화 */}
+        </div>
+    </Page>
+)
