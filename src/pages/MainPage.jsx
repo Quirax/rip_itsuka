6261631ae6a73f2dea63e2f64b0dd950acbe7d33
Quirax Lee <quiraxical@gmail.com>
2025-09-05 17:27:46 +0900

    Placed images

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,16 +1,28 @@
 import Page from '../components/Page'
+import Chrysanthemum from '/chrysanthemum.png'
+import Character from '/character.png' // TODO: 이츠카 시네로 변경 (현재 임시로 세피로트 사용)
 
 export default () => (
-    <Page className='bg-black'>
-        <div>
+    <Page className='bg-black flex flex-col h-screen'>
+        <div className='m-12 text-center space-y-4 flex-none'>
             {/* Top */}
-            <h1>謹弔</h1>
-            <h2>삼가 고인의 명복을 빕니다</h2>
+            <h1 className='text-8xl'>謹弔</h1>
+            <h2 className='text-3xl'>삼가 고인의 명복을 빕니다</h2>
         </div>
-        <div>
+        <div className='flex-1 overflow-hidden relative'>
             {/* Bottom */}
-            {/* TODO: 이미지 추가 - 이츠카 시네 */}
-            {/* TODO: 이미지 추가 - 국화 */}
+            <div className='h-full w-full'>
+                <img
+                    className='m-auto h-full'
+                    src={Character}
+                />
+            </div>
+            <div className='h-1/2 w-full absolute bottom-0'>
+                <img
+                    className='h-full m-auto'
+                    src={Chrysanthemum}
+                />
+            </div>
         </div>
     </Page>
 )
