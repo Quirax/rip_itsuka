f3e17636255cde1bd7a72d755a51d7fa607af09e
Quirax Lee <quiraxical@gmail.com>
2025-09-07 13:30:00 +0900

    Created funeral info page and placed content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Info from './pages/Info'
 import MainPage from './pages/MainPage'
 
 function App() {
     return (
         <>
             <MainPage />
+            <Info />
         </>
     )
 }
